f29420cb41660277193018e91593207288560ddd
Denny Petersson <petersson.denny@gmail.com>
2022-11-14 20:37:16 +0100

    changing english to swedish on buttons

--- src/components/product/ProductSlideover.js
+++ src/components/product/ProductSlideover.js
@@ -172,14 +172,14 @@ const ProductSlideover = ({ open, setOpen, productId, onSave }) => {
                         className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         onClick={handleClose}
                       >
-                        Cancel
+                        Avbryt
                       </button>
                       <button
                         type="submit"
                         className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         onClick={handleSave}
                       >
-                        Save
+                        Spara
                       </button>
                     </div>
                   </div>
