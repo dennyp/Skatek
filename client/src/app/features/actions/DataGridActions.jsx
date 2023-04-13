4703b50b59cf0e4cd63f728ec02f4d7051763095
Denny Petersson <petersson.denny@gmail.com>
2023-04-13 19:20:13 +0200

    adding download to actions, datagrid to documents

--- client/src/app/features/actions/DataGridActions.jsx
+++ client/src/app/features/actions/DataGridActions.jsx
@@ -1,25 +1,57 @@
-import { Delete, Edit } from '@mui/icons-material'
+import { Delete, Edit, GetApp } from '@mui/icons-material'
 import { Box, IconButton, Tooltip } from '@mui/material'
+import { useSelector } from 'react-redux'
+import { selectCurrentIsAdmin } from '../auth/authSlice'
 
-const DataGridActions = ({ params, onEditClick, onDeleteClick }) => {
-  let deleteAction
-  if (onDeleteClick !== undefined) {
-    deleteAction = (
-      <Tooltip title="Radera">
-        <IconButton onClick={() => onDeleteClick(params.row)}>
-          <Delete />
+const DataGridActions = ({
+  params,
+  onEditClick,
+  onDeleteClick,
+  onDownloadClick,
+}) => {
+  const isAdmin = useSelector(selectCurrentIsAdmin)
+
+  let downloadAction
+  if (onDownloadClick !== undefined) {
+    downloadAction = (
+      <Tooltip title="Ladda ned">
+        <IconButton onClick={() => onDownloadClick(params.row)}>
+          <GetApp />
         </IconButton>
       </Tooltip>
     )
   }
 
+  let editAction
+  if (onEditClick !== undefined) {
+    if (isAdmin) {
+      editAction = (
+        <Tooltip title="Ändra">
+          <IconButton onClick={() => onEditClick(params.row)}>
+            <Edit />
+          </IconButton>
+        </Tooltip>
+      )
+    }
+  }
+
+  let deleteAction
+  if (onDeleteClick !== undefined) {
+    if (isAdmin) {
+      deleteAction = (
+        <Tooltip title="Radera">
+          <IconButton onClick={() => onDeleteClick(params.row)}>
+            <Delete />
+          </IconButton>
+        </Tooltip>
+      )
+    }
+  }
+
   return (
     <Box>
-      <Tooltip title="Ändra">
-        <IconButton onClick={() => onEditClick(params.row)}>
-          <Edit />
-        </IconButton>
-      </Tooltip>
+      {downloadAction}
+      {editAction}
       {deleteAction}
     </Box>
   )
--- client/src/app/features/documents/DocumentList.jsx
+++ client/src/app/features/documents/DocumentList.jsx
@@ -1,22 +1,55 @@
 // import GetApp from '@mui/icons-material/GetApp'
 import InsertDriveFile from '@mui/icons-material/InsertDriveFile'
-import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
+import { Box, List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
 import React from 'react'
-import DocumentDownloadButton from './DocumentDownloadButton'
+import { useDispatch } from 'react-redux'
+import DataGridActions from '../actions/DataGridActions'
+import { downloadFile } from './documentSlice'
 
 const DocumentList = ({ files }) => {
+  const dispatch = useDispatch()
+
+  const onDownloadClick = (row) => {
+    dispatch(downloadFile({ id: row._id, name: row.name }))
+  }
+
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Namn',
+      flex: 1,
+      renderCell: (params) => {
+        const name = params.row?.name
+        return (
+          <>
+            <InsertDriveFile className="mr-2" />
+            {name}
+          </>
+        )
+      },
+    },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      type: 'actions',
+      flex: 0.1,
+      renderCell: (params) => (
+        <DataGridActions {...{ params, onDownloadClick }} />
+      ),
+    },
+  ]
+
   return (
-    <List>
-      {files.map((file, index) => (
-        <ListItem key={index}>
-          <ListItemIcon>
-            <InsertDriveFile />
-          </ListItemIcon>
-          <ListItemText primary={file.name} />
-          <DocumentDownloadButton id={file._id} name={file.name} />
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Box sx={{ height: '80vh', m: '1.5rem 1rem' }}>
+        <DataGrid
+          getRowId={(file) => file._id}
+          rows={files}
+          columns={columns}
+        />
+      </Box>
+    </>
   )
 }
 
