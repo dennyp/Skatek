bbb5911f42881e056cb8a22beb597cd4e160a18b
Denny Petersson <petersson.denny@gmail.com>
2023-04-17 19:13:24 +0200

    refactoring form content

---
+++ client/src/app/features/lighttraplogs/LightTrapLogFormContent.jsx
@@ -0,0 +1,149 @@
+import { Box, CircularProgress } from '@mui/material'
+import React, { useEffect } from 'react'
+import NumberInputGroup from '../../../components/NumberInputGroup'
+import TextInputGroup from '../../../components/TextInputGroup'
+import ProductInputGroup from '../products/ProductInputGroup'
+import { useGetLightTrapLogQuery } from './lightTrapLogsApiSlice'
+
+const LightTrapLogFormContent = ({ id, setIsChanged, content, setContent }) => {
+  const { isLoading, isSuccess, data: log } = useGetLightTrapLogQuery(id)
+
+  useEffect(() => {
+    if (isSuccess) {
+      setContent(log)
+    }
+  }, [isSuccess, log, setContent])
+
+  const handleProductChange = (product) => {
+    setContent({ ...content, product })
+    setIsChanged(true)
+  }
+
+  const handleCommentChange = (comment) => {
+    setContent({ ...content, comment })
+    setIsChanged(true)
+  }
+
+  const handleDateLoggedChange = (event) => {
+    setContent({ ...content, dateLogged: event.target.value })
+    setIsChanged(true)
+  }
+
+  const handleFlyActivityChange = (flyActivity) => {
+    setContent({ ...content, flyActivity })
+    setIsChanged(true)
+  }
+
+  const handlebananaflyActivityChange = (bananaflyActivity) => {
+    setContent({ ...content, bananaflyActivity })
+    setIsChanged(true)
+  }
+
+  const handleWaspActivityChange = (waspActivity) => {
+    setContent({ ...content, waspActivity })
+    setIsChanged(true)
+  }
+
+  const handleNeuropteranActivityChange = (neuropteranActivity) => {
+    setContent({ ...content, neuropteranActivity })
+    setIsChanged(true)
+  }
+
+  const handleDaddylonglegsActivityChange = (daddylonglegsActivity) => {
+    setContent({ ...content, daddylonglegsActivity })
+    setIsChanged(true)
+  }
+
+  const handleMiscActivityChange = (miscActivity) => {
+    setContent({ ...content, miscActivity })
+    setIsChanged(true)
+  }
+
+  let formContent
+  if (isLoading) {
+    formContent = (
+      <>
+        <Box className="flex justify-center">
+          <CircularProgress className="mt-20" size={84} />
+        </Box>
+      </>
+    )
+  } else {
+    formContent = (
+      <>
+        <div className="space-y-1 px-4 sm:space-y-0 sm:px-6 sm:py-5">
+          <ProductInputGroup
+            value={content?.product}
+            department={content?.product?.department}
+            onChange={handleProductChange}
+            onlyLightTraps={true}
+          />
+        </div>
+        <div className="space-y-1 px-4 sm:space-y-0 sm:px-6 sm:py-5">
+          <div className="columns-2">
+            <NumberInputGroup
+              label="Flugor"
+              value={content?.flyActivity}
+              onChange={handleFlyActivityChange}
+            />
+            <NumberInputGroup
+              label="Bananflugor"
+              value={content?.bananaflyActivity}
+              onChange={handlebananaflyActivityChange}
+            />
+          </div>
+          <div className="columns-2">
+            <NumberInputGroup
+              label="Getingar"
+              value={content?.waspActivity}
+              onChange={handleWaspActivityChange}
+            />
+            <NumberInputGroup
+              label="Nätvingar"
+              value={content?.neuropteranActivity}
+              onChange={handleNeuropteranActivityChange}
+            />
+          </div>
+          <div className="columns-2">
+            <NumberInputGroup
+              label="Harkrankar"
+              value={content?.daddylonglegsActivity}
+              onChange={handleDaddylonglegsActivityChange}
+            />
+            <NumberInputGroup
+              label="Övrigt"
+              value={content?.miscActivity}
+              onChange={handleMiscActivityChange}
+            />
+          </div>
+        </div>
+        <div className="space-y-1 px-4 sm:space-y-0 sm:px-6 sm:py-5">
+          <TextInputGroup
+            label="Kommentar"
+            value={content?.comment}
+            onChange={handleCommentChange}
+          />
+        </div>
+        <div className="space-y-1 px-4 sm:space-y-0 sm:px-6 sm:py-5">
+          <label
+            htmlFor="date-logged"
+            className="block text-xs font-medium text-gray-900"
+          >
+            Loggad datum
+          </label>
+          <input
+            id="date-logged"
+            type="date"
+            value={content?.dateLogged.slice(0, 10)}
+            onChange={handleDateLoggedChange}
+            className="rounded-md border-gray-300"
+          />
+        </div>
+      </>
+    )
+  }
+
+  return formContent
+}
+
+export default LightTrapLogFormContent
--- client/src/app/features/lighttraplogs/LightTrapLogSlideover.jsx
+++ client/src/app/features/lighttraplogs/LightTrapLogSlideover.jsx
@@ -1,51 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { toast } from 'react-toastify'
-import NumberInputGroup from '../../../components/NumberInputGroup'
 import SlideoverLayout from '../../../components/SlideoverLayout'
-import TextInputGroup from '../../../components/TextInputGroup'
-import ProductInputGroup from '../products/ProductInputGroup'
-import {
-  useGetLightTrapLogQuery,
-  useUpdateLightTrapLogMutation,
-} from './lightTrapLogsApiSlice'
+import LightTrapLogFormContent from './LightTrapLogFormContent'
+import { useUpdateLightTrapLogMutation } from './lightTrapLogsApiSlice'
 
 const LightTrapLogSlideover = ({ id, open, setOpen }) => {
   const [isChanged, setIsChanged] = useState(false)
-  const [product, setProduct] = useState({})
-  const [comment, setComment] = useState('')
-  const [dateLogged, setDateLogged] = useState('')
-  const [flyActivity, setFlyActivity] = useState(0)
-  const [bananaflyActivity, setBananaflyActivity] = useState(0)
-  const [waspActivity, setWaspActivity] = useState(0)
-  const [neuropteranActivity, setNeuropteranActivity] = useState(0)
-  const [daddylonglegsActivity, setDaddylonglegsActivity] = useState(0)
-  const [miscActivity, setMiscActivity] = useState(0)
+
+  const [content, setContent] = useState({
+    flyActivity: 0,
+    bananaflyActivity: 0,
+    waspActivity: 0,
+    neuropteranActivity: 0,
+    daddylonglegsActivity: 0,
+    miscActivity: 0,
+    comment: '',
+    dateLogged: '',
+    product: {},
+  })
 
   const successMessage = () => toast.success('Logg sparad')
   const failureMessage = () => toast.error('Logg kunde inte sparas')
   const noChangeMessage = () => toast.warn('Ingen ändring att spara')
 
   const [updateLightTrapLog, { isLoading }] = useUpdateLightTrapLogMutation()
-
-  const {
-    isLoading: isLogLoading,
-    isSuccess,
-    data: log,
-  } = useGetLightTrapLogQuery(id)
-
-  useEffect(() => {
-    if (isSuccess) {
-      setComment(log?.comment)
-      setDateLogged(log?.dateLogged.slice(0, 10))
-      setProduct(log?.product)
-      setFlyActivity(log?.flyActivity)
-      setBananaflyActivity(log?.bananaflyActivity)
-      setWaspActivity(log?.waspActivity)
-      setNeuropteranActivity(log?.neuropteranActivity)
-      setDaddylonglegsActivity(log?.daddylonglegsActivity)
-      setMiscActivity(log?.miscActivity)
-    }
-  }, [isSuccess, log])
 
   const handleSave = async () => {
     try {
@@ -56,28 +34,21 @@ const LightTrapLogSlideover = ({ id, open, setOpen }) => {
 
       const response = await updateLightTrapLog({
         id,
-        product: product._id,
-        comment,
-        dateLogged,
-        flyActivity: parseInt(flyActivity),
-        bananaflyActivity: parseInt(bananaflyActivity),
-        waspActivity: parseInt(waspActivity),
-        neuropteranActivity: parseInt(neuropteranActivity),
-        daddylonglegsActivity: parseInt(daddylonglegsActivity),
-        miscActivity: parseInt(miscActivity),
+        product: content?.product._id,
+        comment: content?.comment,
+        dateLogged: content?.dateLogged.slice(0, 10),
+        flyActivity: parseInt(content?.flyActivity),
+        bananaflyActivity: parseInt(content?.bananaflyActivity),
+        waspActivity: parseInt(content?.waspActivity),
+        neuropteranActivity: parseInt(content?.neuropteranActivity),
+        daddylonglegsActivity: parseInt(content?.daddylonglegsActivity),
+        miscActivity: parseInt(content?.miscActivity),
       }).unwrap()
 
       if (response) {
         successMessage()
         setIsChanged(false)
         setOpen(false)
-        setComment('')
-        setFlyActivity(0)
-        setBananaflyActivity(0)
-        setWaspActivity(0)
-        setNeuropteranActivity(0)
-        setDaddylonglegsActivity(0)
-        setMiscActivity(0)
       }
     } catch (error) {
       console.error('error saving the updated activity log', error)
@@ -85,131 +56,22 @@ const LightTrapLogSlideover = ({ id, open, setOpen }) => {
     }
   }
 
-  const handleProductChange = (product) => {
-    setProduct(product)
-    setIsChanged(true)
-  }
-
-  const handleCommentChange = (changedComment) => {
-    setComment(changedComment)
-    setIsChanged(true)
-  }
-
-  const handleDateLoggedChange = (event) => {
-    setDateLogged(event.target.value)
-    setIsChanged(true)
-  }
-
-  const handleFlyActivityChange = (flyActivity) => {
-    setFlyActivity(flyActivity)
-    setIsChanged(true)
-  }
-
-  const handlebananaflyActivityChange = (bananaflyActivity) => {
-    setBananaflyActivity(bananaflyActivity)
-    setIsChanged(true)
-  }
-
-  const handleWaspActivityChange = (waspActivity) => {
-    setWaspActivity(waspActivity)
-    setIsChanged(true)
-  }
-
-  const handleNeuropteranActivityChange = (neuropteranActivity) => {
-    setNeuropteranActivity(neuropteranActivity)
-    setIsChanged(true)
-  }
-
-  const handleDaddylonglegsActivityChange = (daddylonglegsActivity) => {
-    setDaddylonglegsActivity(daddylonglegsActivity)
-    setIsChanged(true)
-  }
-
-  const handleMiscActivityChange = (miscActivity) => {
-    setMiscActivity(miscActivity)
-    setIsChanged(true)
-  }
-
   return (
     <>
-      {log && !isLogLoading && (
-        <SlideoverLayout
-          open={open}
-          setOpen={setOpen}
-          isLoading={isLoading}
-          handleSave={handleSave}
-          title="Ändra logg för ljusfälla"
-        >
-          <div className="space-y-1 px-4 sm:space-y-0 sm:px-6 sm:py-5">
-            <ProductInputGroup
-              value={product}
-              department={product?.department}
-              onChange={handleProductChange}
-              onlyLightTraps={true}
-            />
-          </div>
-          <div className="space-y-1 px-4 sm:space-y-0 sm:px-6 sm:py-5">
-            <div className="columns-2">
-              <NumberInputGroup
-                label="Flugor"
-                value={flyActivity}
-                onChange={handleFlyActivityChange}
-              />
-              <NumberInputGroup
-                label="Bananflugor"
-                value={bananaflyActivity}
-                onChange={handlebananaflyActivityChange}
-              />
-            </div>
-            <div className="columns-2">
-              <NumberInputGroup
-                label="Getingar"
-                value={waspActivity}
-                onChange={handleWaspActivityChange}
-              />
-              <NumberInputGroup
-                label="Nätvingar"
-                value={neuropteranActivity}
-                onChange={handleNeuropteranActivityChange}
-              />
-            </div>
-            <div className="columns-2">
-              <NumberInputGroup
-                label="Harkrankar"
-                value={daddylonglegsActivity}
-                onChange={handleDaddylonglegsActivityChange}
-              />
-              <NumberInputGroup
-                label="Övrigt"
-                value={miscActivity}
-                onChange={handleMiscActivityChange}
-              />
-            </div>
-          </div>
-          <div className="space-y-1 px-4 sm:space-y-0 sm:px-6 sm:py-5">
-            <TextInputGroup
-              label="Kommentar"
-              value={comment}
-              onChange={handleCommentChange}
-            />
-          </div>
-          <div className="space-y-1 px-4 sm:space-y-0 sm:px-6 sm:py-5">
-            <label
-              htmlFor="date-logged"
-              className="block text-xs font-medium text-gray-900"
-            >
-              Loggad datum
-            </label>
-            <input
-              id="date-logged"
-              type="date"
-              value={dateLogged}
-              onChange={handleDateLoggedChange}
-              className="rounded-md border-gray-300"
-            />
-          </div>
-        </SlideoverLayout>
-      )}
+      <SlideoverLayout
+        open={open}
+        setOpen={setOpen}
+        isLoading={isLoading}
+        handleSave={handleSave}
+        title="Ändra logg för ljusfälla"
+      >
+        <LightTrapLogFormContent
+          id={id}
+          setIsChanged={setIsChanged}
+          content={content}
+          setContent={setContent}
+        />
+      </SlideoverLayout>
     </>
   )
 }
